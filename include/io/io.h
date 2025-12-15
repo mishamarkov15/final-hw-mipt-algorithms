3c10f1660de0b558acbee0160ecc68bfc39acf99
Михаил Марков <mikhaiil@mmarkovv.ru>
2025-12-15 06:45:37 +0300

    Written docx

--- include/io/io.h
+++ include/io/io.h
@@ -7,6 +7,18 @@
 #include "../analyzer.h"
 
 namespace analyzer::io {
+    /**
+     * @brief Считывает список товаров из файла и преобразует их в токены.
+     *
+     * Ожидается формат строки:
+     * ID<SPACE>Название товара
+     *
+     * При включённой предобработке выполняется
+     * нормализация текстовых данных.
+     *
+     * @param filename Имя входного файла.
+     * @return Отображение: ID товара → множество токенов.
+     */
     std::unordered_map<std::string, TokenSet>
     read_items(const std::string &filename);
 } // analyzer::io
